b03ddf2c8ca2ef845950dd000a615b62fae33791
LeoKenny <leo.treicheldacunha@gmail.com>
2024-06-18 04:59:11 -0300

    feat: yank highligh and linting config update

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 require("kenny.core")
 require("kenny.lazy")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
